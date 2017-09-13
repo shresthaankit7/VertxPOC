d45b6693bc56a2f761f9bc8e3163dec03ffaf3c7
ankit_shrestha <ankshrestha@system.deerwalk.com>
2017-09-13 17:36:55 +0545

    Pushing test Branch

--- FileUploader/src/main/java/io/vertx/FileUploader/Server.java
+++ FileUploader/src/main/java/io/vertx/FileUploader/Server.java
@@ -1,7 +1,6 @@
 package io.vertx.FileUploader;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Vertx;
 import io.vertx.core.file.AsyncFile;
 import io.vertx.core.file.OpenOptions;
 import io.vertx.core.http.HttpServer;
@@ -17,7 +16,7 @@ import java.util.UUID;
 public class Server extends AbstractVerticle {
 
     public void start(){
-
+        System.out.println("TEST LINE !!!");
         Router router = Router.router(vertx);
 
         router.get("/fileupload").handler(routingContext ->{
