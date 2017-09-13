29ef7364e0e65af78341cbecd53c0f9c4641ddcd
ankit_shrestha <ankshrestha@system.deerwalk.com>
2017-09-13 17:39:06 +0545

    Pushing P_A branch

--- FileUploader/src/main/java/io/vertx/FileUploader/Server.java
+++ FileUploader/src/main/java/io/vertx/FileUploader/Server.java
@@ -1,7 +1,6 @@
 package io.vertx.FileUploader;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Vertx;
 import io.vertx.core.file.AsyncFile;
 import io.vertx.core.file.OpenOptions;
 import io.vertx.core.http.HttpServer;
@@ -17,6 +16,8 @@ import java.util.UUID;
 public class Server extends AbstractVerticle {
 
     public void start(){
+
+        System.out.println("P_A");
 
         Router router = Router.router(vertx);
 
