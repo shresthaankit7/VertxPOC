75133e02d68f4b8669a96541ccc9795d1817d2af
shresthaankit7 <shresthaankit7@gmail.com>
2017-10-29 16:55:37 +0545

    DBClient Addded.

---
+++ rxJavaMongoImpl/src/main/java/rx/java/Database/DBClient.java
@@ -0,0 +1,33 @@
+package rx.java.Database;
+
+import io.vertx.core.Vertx;
+import io.vertx.core.json.JsonObject;
+import io.vertx.ext.jdbc.JDBCClient;
+import io.vertx.ext.mongo.MongoClient;
+import io.vertx.ext.mongo.impl.MongoClientImpl;
+import rx.Observable;
+
+/**
+ * Created by ankit07 on 9/18/16.
+ */
+public class DBClient {
+
+    public static MongoClient mongoClient;
+
+    public static JDBCClient jdbcClient;
+
+    public DBClient(Vertx vertx, JsonObject config){
+        System.out.println("DB CLIENT IN BRANCH1 ");
+        JsonObject c = new JsonObject()
+                .put("driver_class","com.mysql.jdbc.Driver")
+                .put("url", "jdbc:mysql://localhost:3306/first")
+                .put("user","root")
+                .put("password","asdf123");
+
+        this.jdbcClient = JDBCClient.createShared(vertx,c);
+    }
+
+    public static MongoClient getMongoClient(){
+        return mongoClient;
+    }
+}
