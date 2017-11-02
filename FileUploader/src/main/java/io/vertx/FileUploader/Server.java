ff62813a1962ce729eae5e1fc270c2a7965ec951
ankit_shrestha <ankshrestha@system.deerwalk.com>
2017-11-02 18:16:55 +0545

    ADDED one line

--- FileUploader/src/main/java/io/vertx/FileUploader/Server.java
+++ FileUploader/src/main/java/io/vertx/FileUploader/Server.java
@@ -1,51 +1,23 @@
 package io.vertx.FileUploader;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Vertx;
-import io.vertx.core.file.AsyncFile;
-import io.vertx.core.file.OpenOptions;
-import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerResponse;
-import io.vertx.core.streams.Pump;
-import io.vertx.ext.web.Router;
-
-import java.util.UUID;
 
 /**
  * Created by ankit07 on 5/3/16.
  */
 public class Server extends AbstractVerticle {
 
+    static String contentType = "Content-Type";
+    static String contentValue = "application/json";
+
     public void start(){
 
-        System.out.println("P_A");
+        for (Integer p = 0; p < 5; p++) {
 
-        Router router = Router.router(vertx);
+            System.out.println("ADDING THIS LINE");
+            System.out.println("/dev/sd" + (char) (p + 102));
 
-        router.get("/fileupload").handler(routingContext ->{
-
-            HttpServerResponse response = routingContext.response();
-
-            response.putHeader("context-type","text/plain");
-            response.setStatusCode(200);
-            response.end("Resquest got");
-        });
-
-        HttpServer httpServer = vertx.createHttpServer().requestHandler(req -> {
-            req.pause();
-            String filename = UUID.randomUUID() + ".csv";
-            vertx.fileSystem().open(filename, new OpenOptions(), ares -> {
-                AsyncFile file = ares.result();
-                Pump pump = Pump.pump(req, file);
-                req.endHandler(v1 -> file.close(v2 -> {
-                    System.out.println("Uploaded to " + filename);
-                    req.response().end();
-                }));
-                pump.start();
-                req.resume();
-            });
-        });
-        httpServer.requestHandler(router::accept).listen(8080);
+        }
 
     }
 }
